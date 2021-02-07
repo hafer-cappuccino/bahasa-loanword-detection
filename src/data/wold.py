3584bc380fc05779fd5a33b76a7d2d76e911482b
Steph Samson <hello@stephsamson.com>
2021-02-08 00:53:07 +0100

    Add bonobo for ETL.

--- src/data/wold.py
+++ src/data/wold.py
@@ -1,3 +1,21 @@
+from typing import List, Tuple
+
+import bonobo
 from pycldf.dataset import Dataset
 
+
 dataset: Dataset = Dataset.from_metadata('lib/wold/cldf/cldf-metadata.json')
+
+
+def extract():
+    forms = list(dataset['FormTable'])
+    for form in forms:
+        yield form
+
+def transform(form: Tuple[str, str]):
+    if form['Language_ID'] == 'Indonesian':
+        print(form['Form'])
+
+if __name__ == '__main__':
+    graph = bonobo.Graph(extract, transform)
+    bonobo.run(graph)
