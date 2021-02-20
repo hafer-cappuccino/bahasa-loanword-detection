045887a774b65b3093ffcb4c888b74532844910b
Steph Samson <hello@stephsamson.com>
2021-02-20 12:59:36 +0100

    Test housekeeping.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
+
 from src.data.wold import dataset
 
 
-@pytest.fixture(scope='function')
-def wold_cldf():
-    return dataset
+
+@pytest.fixture(scope='module')
+def wold_cldf_wordforms():
+    return list(dataset['FormTable'])[100]
--- tests/test_wold.py
+++ tests/test_wold.py
@@ -1,3 +1,19 @@
-def test_dataset(wold_cldf):
-    assert wold_cldf.module == 'Wordlist'
-    assert wold_cldf.properties['dc:identifier'] == 'http://wold.clld.org'
+from types import GeneratorType
+
+from src.data.wold import WordForm, dataset, extract_words
+
+
+def test_dataset():
+    assert dataset.module == 'Wordlist'
+    assert dataset.properties['dc:identifier'] == 'http://wold.clld.org'
+
+
+def test_pipeline():
+    extracted_words = extract_words()
+    assert isinstance(extracted_words, GeneratorType)
+
+    assert isinstance(list(extracted_words)[0], WordForm)
+
+    for word in extracted_words:
+        form = list(filter_indonesian_loanwords(extracted_words(word))).pop()
+        assert form.language == 'Indonesian'
