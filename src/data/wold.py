62d255374d45c452619df9720ed778e5fbdf788f
Steph Samson <hello@stephsamson.com>
2021-02-28 20:05:59 +0100

    Fix lambda.

--- src/data/wold.py
+++ src/data/wold.py
@@ -6,21 +6,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 
-from src.data.models import Dataset as Dataset
-
 
 @dataclass
 class WOLD:
     csv_path: str = 'out/indonesian_wordforms.csv'
-    df: pd.Dataframe = None
+    df: pd.DataFrame = None
     encoder: MultiLabelBinarizer = MultiLabelBinarizer()
 
 
     def __post_init__(self):
         self.df = pd.read_csv(self.csv_path)
-        self.df['segments'] = self.df['segments'].apply(lambda x: ast.literal_eval)
-
-        self.df['segments'] = self.df['segments']
+        self.df['segments'] = self.df['segments'].apply(lambda x: ast.literal_eval(x))
 
         # only consider borrowing scores 1, everything else is *not* a loanword
         self.df.loc[self.df.borrowing_score != 1.0, 'borrowing_score'] = 0
@@ -55,29 +51,28 @@ class WOLD:
                 processed.append(segment)
         return processed
 
-    
 
 wold_data = WOLD()
 
 
 # The data to feed into the Markov LM.
-BagOfSounds = Dataset(
-    *train_test_split(
-        wold_data.df[['value', 'borrowing_score']],
-        wold_data.df.borrowing_score,
-        test_size=0.2,
-        random_state=42,
-    )
-)
+# WOLDDataset = Dataset(
+#     *train_test_split(
+#         wold_data.df[['value', 'borrowing_score']],
+#         wold_data.df.borrowing_score,
+#         test_size=0.2,
+#         random_state=42,
+#     )
+# )
 
 
 # The data to feed into the SVM.
 # ref: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-BagOfSoundsOneHot = Dataset(
-    *train_test_split(
-        wold_data.onehot(),
-        wold_data.df.borrowing_score,
-        test_size=0.2,
-        random_state=42,
-    )
-)
+# BagOfSounds = Dataset(
+#     *train_test_split(
+#         wold_data.onehot(),
+#         wold_data.df.borrowing_score,
+#         test_size=0.2,
+#         random_state=42,
+#     )
+# )
